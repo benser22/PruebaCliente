6dea89e3ea726253d23de2b2199a80dfbef0110f
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-01-29 18:59:11 -0300

    Deployment FrontEnd

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { List, ListItem, Typography, Box } from "@mui/material";
 import UpdateItem from "./UpdateItem";
 import DeleteItem from "./DeleteItem";
 import useStore from "../store";
 
-const ItemList = ({ items }) => {
+const ItemList = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const updateItem = useStore((state) => state.updateItem);
   const deleteItem = useStore((state) => state.deleteItem);
 
-  React.useEffect(() => {
-    fetchData()
-      .then((data) => {
+  useEffect(() => {
+    // Lógica para obtener los datos
+    const fetchData = async () => {
+      try {
+        const response = await fetch("tu_url_de_api");
+        const data = await response.json();
         setItems(data);
-        setLoading(false);
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error("Error al obtener datos:", error);
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+
+    fetchData();
   }, []);
-  
+
   if (loading) {
     return <Typography>Cargando...</Typography>;
   }
@@ -27,7 +35,7 @@ const ItemList = ({ items }) => {
   return (
     <Box sx={{ margin: "auto", marginTop: "1rem", maxWidth: 400 }}>
       <List>
-        {items?.map((item) => (
+        {items.map((item) => (
           <ListItem key={item.id}>
             <Typography
               key={`typography-${item.id}`}
